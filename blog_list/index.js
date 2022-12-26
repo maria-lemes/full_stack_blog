5b568990b6fcb7b93ad81fe5e52b19801fc4425b
maria-lemes <madulemes10@gmail.com>
2022-12-26 15:28:24 -0300

    4.2 controllers and middleware

--- blog_list/index.js
+++ blog_list/index.js
@@ -1,48 +1,28 @@
-
-const http = require('http')
-const express = require('express')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
+const express = require('express')
 const app = express()
 const cors = require('cors')
-//const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const http = require('http')
 
-const Blog = require('./models/blog')
 
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// })
 
-// const Blog = mongoose.model('Blog', blogSchema)
+//const Blog = require('./models/blog')
 
-// const mongoUrl = config.MONGODB_URI
-// mongoose.connect(mongoUrl)
+
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
+
+module.exports = app
