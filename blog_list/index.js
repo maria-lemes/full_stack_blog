ca621ae089d5da4293aa3aee49ed5afec539885f
maria-lemes <madulemes10@gmail.com>
2022-12-26 14:45:32 -0300

    4.2 utils

--- blog_list/index.js
+++ blog_list/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
 
 const http = require('http')
 const express = require('express')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -15,7 +16,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -39,7 +40,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
