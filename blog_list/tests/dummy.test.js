058118584a30b26a07a543f127a8a29a50d4efe3
maria-lemes <madulemes10@gmail.com>
2022-12-26 16:21:43 -0300

    4.4

--- blog_list/tests/dummy.test.js
+++ blog_list/tests/dummy.test.js
@@ -1,14 +1,5 @@
-const dummy = require('../utils/list_helper')
-// const Blog = require('./models/blog')
-// const blogs = Blog.find({})
-
-// test('dummy test', () => {
-//     const result = dummy(blogs)
-  
-//     expect(result).toBe(1)
-// })
-
 const listHelper = require('../utils/list_helper')
+const blogs = require('./blogs_list')
 
 test('dummy returns one', () => {
   const blogs = []
@@ -16,3 +7,27 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+  
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+
+    test('in a list of blogs',()=> {
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(36)
+    })
+
+  })
