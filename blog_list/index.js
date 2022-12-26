7a98dff0e919aa8d893b2f74d1a374271236a080
maria-lemes <madulemes10@gmail.com>
2022-12-26 15:34:47 -0300

    4.2 full app

--- blog_list/index.js
+++ blog_list/index.js
@@ -1,28 +1,10 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
+const app = require('./app') 
 const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
 
-
-//const Blog = require('./models/blog')
-
-
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
-
-module.exports = app
