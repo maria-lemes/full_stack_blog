b86f293709692c351e8860efcc41adf6f948b677
maria-lemes <madulemes10@gmail.com>
2022-12-27 01:47:37 -0300

    4.10

--- blog_list/tests/blogs_api.test.js
+++ blog_list/tests/blogs_api.test.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
-const blogs = require('./blogs_list')
+const helper = require('./blog_test_helper')
 
 const api = supertest(app)
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    blogs.map(async (blog) =>{
-        let blogObject = new Blog(blog)
-        await blogObject.save()
-    })
-})
+    let blogObject = new Blog(helper.initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(helper.initialBlogs[1])
+    await blogObject.save()
+},10000)
 
 
 
@@ -25,9 +25,35 @@ test('blog list is returned as json', async () => {
 
 test('blog list returns all blogs', async () => {
         const response = await api.get('/api/blogs')
+        
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+        
+})
+
+test('post creates a new blog', async () => {
+    const newBlog = {
+        title: "test blog",
+        author: "Me",
+        url: "Me.com",
+        likes: 123
+    }
     
-        expect(response.body).toHaveLength(blogs.length)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+      const totalBlogsEnd = await helper.blogsInDb()
+    
+      const titles = totalBlogsEnd.map(r => r.title)
+    
+      expect(totalBlogsEnd).toHaveLength(helper.initialBlogs.length + 1)
+      expect(titles).toContain("test blog")   
+    
+
 }, 100000)
+
 
 
 afterAll(() => {
--- blog_list/tests/dummy.test.js
+++ blog_list/tests/dummy.test.js
@@ -1,5 +1,5 @@
 const listHelper = require('../utils/list_helper')
-const blogs = require('./blogs_list')
+const blogs = require('./blog_test_helper')
 
 test('dummy returns one', () => {
   const blogs = []
